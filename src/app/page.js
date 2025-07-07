2b03f82e17a743c4425c2f7361bc548741436e63
codezaryan <aryanpatel7987.com>
2025-07-07 13:37:02 -0400

    initail-docker-next connection

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>
+        Welcome we are deploying our application using docker
+      </h1>
+    </>
+  );
+}
